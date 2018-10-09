9fd63d011edce90aea20eb4a20260a9fa852449f
v-jizhou <v-jizhou@microsoft.com>
2018-10-09 11:21:04 +0800

    Add SDDCAnalysisModule Script File 'GetStorageSubsystem.psm1' And 'GetStorageTier.psm1'

---
+++ CIAnalysis/SDDCAnalysisModule/GetStorageTier.psm1
@@ -0,0 +1,36 @@
+<#
+    The script block be used to parsing PS custom objects.
+    We can make some customized analysis operations for the PS custom objects in this script block.
+    The PS custom objects is stored in the script variable '$AnalyzeObject'.
+#>
+
+Function AnalysisPSObject
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [PSCustomObject[]] $AnalyzeObject,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [string] $DownloadFilePath
+    )
+
+    #
+    # Preview PS Custom Object
+    #
+    Write-Host "[GetStorageTier] begins to be analyzed. `n"  -BackgroundColor Green
+
+    #
+    # Analysis Operations Here
+    #
+    
+
+    Write-Host "[GetStorageTier] was analyzed. `n"  -BackgroundColor Green
+
+    # Output CI Analysis PS Object Set
+    $AnalyzeObject
+}
